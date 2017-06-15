99c2032af1110a7df033df129f50c2286d92c273
Yaroslav Kryvko <yaroslav.kryvko@gmail.com>
2017-06-15 18:52:31 +0300

    Added example on modifying default content controls

---
+++ ControlsModifiedContentControls/src/main/java/com/aol/mobile/sdk/example/controlsmodifiedcontentcontrols/MainActivity.java
@@ -0,0 +1,57 @@
+package com.aol.mobile.sdk.example.controlsmodifiedcontentcontrols;
+
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.aol.mobile.sdk.player.OneSDK;
+import com.aol.mobile.sdk.player.OneSDKBuilder;
+import com.aol.mobile.sdk.player.Player;
+import com.aol.mobile.sdk.player.view.PlayerFragment;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String VIDEO_ID = "577cc23d50954952cc56bc47";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        final PlayerFragment playerFragment = (PlayerFragment) fm.findFragmentById(R.id.player_fragment);
+        playerFragment.setRetainInstance(true);
+
+        new OneSDKBuilder(getApplicationContext())
+                .create(new OneSDKBuilder.Callback() {
+                    @Override
+                    public void onSuccess(@NonNull OneSDK oneSDK) {
+                        useSDK(oneSDK, playerFragment);
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error Creating SDK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void useSDK(@NonNull OneSDK oneSDK, @NonNull final PlayerFragment playerFragment) {
+        oneSDK.createBuilder()
+                .setAutoplay(true)
+                .buildForVideo(VIDEO_ID, new Player.Callback() {
+                    @Override
+                    public void success(@NonNull Player player) {
+                        playerFragment.getBinder().getPlayerView().setVideoControlsView(new ModifiedContentControls(getApplicationContext()));
+                        playerFragment.getBinder().setPlayer(player);
+                    }
+
+                    @Override
+                    public void error(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
---
+++ ControlsModifiedContentControls/src/main/java/com/aol/mobile/sdk/example/controlsmodifiedcontentcontrols/ModifiedContentControls.java
@@ -0,0 +1,33 @@
+package com.aol.mobile.sdk.example.controlsmodifiedcontentcontrols;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.aol.mobile.sdk.controls.view.PlayerControlsView;
+import com.aol.mobile.sdk.controls.viewmodel.PlayerControlsVM;
+
+public class ModifiedContentControls extends PlayerControlsView {
+    public ModifiedContentControls(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    public void render(@NonNull PlayerControlsVM viewModel) {
+        /**
+         * Here you can modify any property of the default content controls you like.
+         * @see PlayerControlsVM class that holds information whether an item should
+         * be visible, enabled, selected or even what text should be displayed on it.
+         * Keep in mind that all changes here are not per video. For example, if you
+         * change video title here, the same title will be displayed for every video.
+         **/
+
+        // Hide seek forward/back 10 seconds buttons.
+        viewModel.isSeekForwardButtonVisible = false;
+        viewModel.isSeekBackButtonVisible = false;
+
+        // Hide video title.
+        viewModel.isTitleVisible = false;
+
+        super.render(viewModel);
+    }
+}
