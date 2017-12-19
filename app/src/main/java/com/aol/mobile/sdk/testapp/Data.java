50de86ce134e76c816d10b5e919561931a02404d
Yaroslav Kryvko <yaroslav.kryvko@gmail.com>
2017-12-19 12:19:43 +0200

    Added tutorial one

---
+++ app/src/main/java/com/aol/mobile/sdk/testapp/Data.java
@@ -0,0 +1,7 @@
+package com.aol.mobile.sdk.testapp;
+
+public class Data {
+    public static final String VIDEO_ID = "577cc23d50954952cc56bc47";
+    public static final String[] ARRAY_OF_VIDEOS = {"593967be9e45105fa1b5939a", "577cc23d50954952cc56bc47", "5939698f85eb427b86aa0a14"};
+    public static final String PLAYLIST_ID = "577cc27b88d2ff0d0f5acc71";
+}
---
+++ app/src/main/java/com/aol/mobile/sdk/testapp/tutorials/two/TutorialTwoActivity.java
@@ -0,0 +1,15 @@
+package com.aol.mobile.sdk.testapp.tutorials.two;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aol.mobile.sdk.testapp.R;
+
+public class TutorialTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_two);
+    }
+}
